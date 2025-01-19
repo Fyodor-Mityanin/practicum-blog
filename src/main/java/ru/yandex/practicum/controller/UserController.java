0276b911b8a183419b6dd948627e2f057cc1f45b
Митянин Федор Юрьевич <f.mityanin@s7.ru>
2025-01-19 17:02:15 +0700

    init

---
+++ src/main/java/ru/yandex/practicum/controller/UserController.java
@@ -0,0 +1,46 @@
+package ru.yandex.practicum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.yandex.practicum.model.User;
+import ru.yandex.practicum.service.UserService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/users") // Контроллер обрабатывает запросы /users
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping // GET запрос /users
+    public String users(Model model) {
+        List<User> users = service.findAll();
+        model.addAttribute("users", users);
+
+        return "users"; // Возвращаем название шаблона — users.html
+    }
+
+    @PostMapping
+    public String save(@ModelAttribute User user) {
+        service.save(user);
+
+        return "redirect:/users"; // Возвращаем страницу, чтобы она перезагрузилась
+    }
+
+    @PostMapping(value = "/{id}", params = "_method=delete")
+    public String delete(@PathVariable(name = "id") Long id) {
+        service.deleteById(id);
+
+        return "redirect:/users";
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.repository;
+
+import ru.yandex.practicum.model.User;
+
+import java.util.List;
+
+public interface UserRepository {
+    List<User> findAll();
+
+    void save(User user);
+
+    void deleteById(Long id);
+}
